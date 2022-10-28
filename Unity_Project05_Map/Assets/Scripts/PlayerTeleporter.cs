91c14c82dc96e537290107a0d869361a283ee470
KasiditMaksai <kasidit.m@kkumail.com>
2022-10-28 21:27:22 +0700

    Map fixed

---
+++ Unity_Project05_Map/Assets/Scripts/PlayerTeleporter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Teleporter"))
+        {
+            currentTeleporter = other.gameObject;
+            Debug.Log("Preessed E to use!");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.gameObject == currentTeleporter)
+        {
+            currentTeleporter = null;
+        }
+    }
+}
